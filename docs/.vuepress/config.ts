06e799c8a6923108b909b319d12085abe0f54944
jdnjk <2394058778@qq.com>
2025-09-20 21:49:31 +0800

    修复无法构建

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,6 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { sitemapPlugin } from '@vuepress/plugin-sitemap'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 export default defineUserConfig({
   
   lang: 'zh-CN',
@@ -58,54 +57,6 @@ export default defineUserConfig({
   plugins: [
     sitemapPlugin({
       hostname: 'https://docs.monday-ovo.top',
-    }),
-    docsearchPlugin({
-      appId: 'VXFWRUIH25',
-      apiKey: '328e575a3431fb48ea09026db382e3ee',
-      indexName: 'monday-ovo-eu',
-      locales: {
-        '/': {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档',
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消',
-              },
-              startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除',
-              },
-              errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接',
-              },
-              footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者',
-              },
-              noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈',
-              },
-            },
-          },
-        },
-      },
     }),
   ],
   bundler: viteBundler(),
