a04f7be7afbddc69261fb06cc05f87e56afed513
jdnjk <2394058778@qq.com>
2025-11-29 10:01:30 +0800

    清理垃圾，升级vp版本，修复搜索

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/workspaces/docs/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-rc.88_vuepress@2.0.0-rc.20_@vuepress+bundler-vite@2.0.0-rc.2_5f5dd3146efdccbbccd2aa75948c62d4/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/workspaces/docs/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-rc.88_vuepress@2.0.0-rc.20_@vuepress+bundler-vite@2.0.0-rc.2_5f5dd3146efdccbbccd2aa75948c62d4/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
--- docs/.vuepress/.temp/pages/guide/help.html.vue
+++
@@ -1,9 +0,0 @@
-<template><div><h1 id="帮助我们" tabindex="-1"><a class="header-anchor" href="#帮助我们"><span>帮助我们</span></a></h1>
-<p>你可以通过<a href="https://afdian.com/a/jdnjk" target="_blank" rel="noopener noreferrer">爱发电</a>来给我们赞助，我们会收到赞助金额*94%，基本我们能收到的时间是次月1日，你也可以使用我们的收款码进行赞助</p>
-<h3 id="jdnjk" tabindex="-1"><a class="header-anchor" href="#jdnjk"><span>jdnjk</span></a></h3>
-<p><img src="@source/guide/wx.png" alt="微信">
-<img src="@source/guide/alipay.png" alt="支付宝">要不先领个红包
-<img src="@source/guide/qrhb.jpg" alt="红包"></p>
-</div></template>
-
-
